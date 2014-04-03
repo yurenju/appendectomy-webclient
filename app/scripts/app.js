df89a04a454ce568fe1d15989cef8f7fccf90b0b
Yuren Ju <yurenju@gmail.com>
2014-04-04 00:56:16 +0800

    added birthday

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'ng-bootstrap3-datepicker'
   ])
   .config(function ($routeProvider) {
     $routeProvider
